0028f3f2f78b65df40bc23e90372a6226f875b43
23030149-coder <23030149@student.bdu.edu.vn>
2026-06-28 16:03:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.image("logo.jpg")
 # TIÊU ĐỀ
 # =============================
 st.title("📈 TRỰC QUAN HÓA GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-st.subheader("TS. VŨ ĐỨC BÌNH")
+st.subheader("Đặng Gia Hân - Đề Tài 9")
 
 st.markdown("---")
 
